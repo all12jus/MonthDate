b30370ff505e88fbf12e4116d51d1caeea95a2bb
Justin Allen <all12jus@gmail.com>
2019-10-12 09:23:30 -0400

    Initial Commit

---
+++ MonthDate WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MonthDate WatchKit Extension
+//
+//  Created by Justin Allen on 10/12/19.
+//  Copyright © 2019 Justin Allen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MonthDate WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  MonthDate WatchKit Extension
+//
+//  Created by Justin Allen on 10/12/19.
+//  Copyright © 2019 Justin Allen. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
